a404f564f81759d9b801f12fd36f374194f5539a
Gaurav Karakoti <karakotigaurav12@gmail.com>
2026-05-16 22:45:59 +0530

    Helper function bhi chlgye

--- src/Utility/Location.js
+++ src/Utility/Location.js
@@ -1,7 +1,42 @@
 export async function getAddressFromCoords(coords) {
-  return '6th Avenue';
+  const response = await fetch(
+    `https://nominatim.openstreetmap.org/reverse?format=json&lat=${coords.lat}&lon=${coords.lng}`
+  );
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch address. Please try again!');
+  }
+
+  const data = await response.json();
+
+  if (data.error) {
+    throw new Error(data.error);
+  }
+
+  return data.display_name;
 }
 
 export async function getCoordsFromAddress(address) {
-  return {lat: 47.01, lng: 33.55}; 
+  const urlEncodedAddress = encodeURI(address);
+
+  const response = await fetch(
+    `https://nominatim.openstreetmap.org/search?format=json&q=${urlEncodedAddress}`
+  );
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch coordinates. Please try again!');
+  }
+
+  const data = await response.json();
+
+  if (!data || data.length === 0) {
+    throw new Error('Could not find coordinates for the specified address.');
+  }
+
+  const bestMatch = data[0];
+
+  return {
+    lat: parseFloat(bestMatch.lat),
+    lng: parseFloat(bestMatch.lon)
+  };
 }
